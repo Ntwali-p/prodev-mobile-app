214184661cc7be99d0c1023502cee3f39e8389eb
Ntwali-p <ntwali.pacific@gmail.com>
2026-01-22 12:21:46 +0200

    changes

--- prodev-mobile-app-0x06/metro.config.js
+++ prodev-mobile-app-0x06/metro.config.js
@@ -1,6 +1,18 @@
+// metro.config.js
+/**
+ * Metro configuration for React Native
+ * https://facebook.github.io/metro/docs/configuration
+ *
+ * Windows-friendly version for NativeWind + Expo Router
+ */
+
 const { getDefaultConfig } = require("expo/metro-config");
 const { withNativeWind } = require("nativewind/metro");
+const path = require("path");
 
 const config = getDefaultConfig(__dirname);
 
-module.exports = withNativeWind(config, { input: "./styles/global.css" });
+// Force Windows-safe path for NativeWind
+module.exports = withNativeWind(config, {
+  input: path.join(__dirname, "styles/global.css"),
+});
